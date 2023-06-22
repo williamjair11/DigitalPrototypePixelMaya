d490015f91dc38a6872876d3d2fbaed5bcf608ef
Renier Gaytan <renier.gaytan@gmail.com>
2023-06-21 19:08:45 -0600

    dummy commit

--- Assets/Scripts/R_Scripts/PlayerSafePoint.cs
+++ Assets/Scripts/R_Scripts/PlayerSafePoint.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class PlayerSafePoint : MonoBehaviour
 {
     [SerializeField] private EnemyMovement _enemyMovement;
-    [SerializeField] private Test _test;
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("greenTorch"))
         {
             _enemyMovement._playerIsInsideGreenLight = true;
-            _test._playerIsInsideGreenLight = true;
         }
     }
     private void OnTriggerExit(Collider other) {
         if (other.CompareTag("greenTorch"))
         {
             _enemyMovement._playerIsInsideGreenLight = false;
-            _test._playerIsInsideGreenLight = false;
         }
     }
 }
